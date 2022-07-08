eaf7f5f665ac74d5935a758ae113648adf27c30a
pascalpuffke <pascal@pascalpuffke.de>
2022-07-08 13:13:34 +0200

    Kernel: Use kprintln() in kassert.cpp

--- src/kernel/kassert.cpp
+++ src/kernel/kassert.cpp
@@ -7,13 +7,13 @@
 void _kassert(const char* file, int line, const char* func, const char* expr)
 {
     Kernel::TTY::set_color(Kernel::VGA::Color::LIGHT_RED, Kernel::VGA::Color::BLACK);
-    kprintf("ASSERTION FAILED: %s\n         in file: %s:%d\n        function: %s\n", expr, file, line, func);
+    kprintln("ASSERTION FAILED: %s\n         in file: %s:%d\n        function: %s", expr, file, line, func);
     Kernel::hang();
 }
 
 void _kassert_msg(const char* file, int line, const char* func, const char* expr, const char* format)
 {
     Kernel::TTY::set_color(Kernel::VGA::Color::LIGHT_RED, Kernel::VGA::Color::BLACK);
-    kprintf("ASSERTION FAILED: %s\n      expression: %s\n         in file: %s:%d\n        function: %s\n", format, expr, file, line, func);
+    kprintln("ASSERTION FAILED: %s\n      expression: %s\n         in file: %s:%d\n        function: %s", format, expr, file, line, func);
     Kernel::hang();
 }
