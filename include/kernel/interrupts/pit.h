f8a1f0ff582819e8b18d2b2616cce702ad2b0a4d
pascalpuffke <pascal@pascalpuffke.de>
2024-06-22 16:21:24 +0200

    Implement the PIT

--- include/kernel/interrupts/pit.h
+++ include/kernel/interrupts/pit.h
@@ -2,14 +2,43 @@
 
 #include <stdlib/types.h>
 
-namespace Kernel::Interrupts {
+namespace Kernel {
 
 class PIT final {
 public:
-    PIT();
+    PIT() = default;
 
-    void enable();
-    void disable();
+    enum class Channel : u8 {
+        IRQ = 0,
+        // 1 is now unused and hardware may not implement it
+        Speaker = 2,
+    };
+
+    enum class AccessMode : u8 {
+        LatchCount,
+        LoByte,
+        HiByte,
+        LoHiByte,
+    };
+
+    enum class OperatingMode : u8 {
+        InterruptOnSignalCount,
+        RetriggerableOneShot,
+        RateGenerator,
+        SquareWaveGenerator,
+        SoftwareTriggeredStrobe,
+        HardwareTriggeredStrobe,
+    };
+
+    enum class BCDBinaryMode : u8 {
+        Binary,
+        BCD,
+    };
+
+    void enable(Channel, AccessMode, OperatingMode, BCDBinaryMode, i32 frequency);
+    void set_count(Channel, u32 count);
+    u32 count_for_frequency(u32 f);
+    u32 read_count(Channel);
 
 private:
 };
--- src/kernel/interrupts/pit.cpp
+++ src/kernel/interrupts/pit.cpp
@@ -1,11 +1,61 @@
 #include <kernel/interrupts/pit.h>
+#include <kernel/io/io.h>
+#include <kernel/util/interrupt_scope.h>
+#include <kernel/util/kassert.h>
+#include <kernel/util/kprintf.h>
 
-namespace Kernel::Interrupts {
+static constexpr u32 PIT_OSCILLATOR_FREQ = 1193182;
+static constexpr u16 PIT_CHANNEL_0 = 0x40;
+static constexpr u16 PIT_CHANNEL_2 = 0x42;
+static constexpr u16 PIT_CMD_REGISTER = 0x43;
 
-void PIT::enable() {
+namespace Kernel {
+
+void PIT::enable(Channel c, AccessMode am, OperatingMode om, BCDBinaryMode bcdbin, i32 frequency) {
+    const auto channel = static_cast<u8>(c);
+    const auto accessMode = static_cast<u8>(am);
+    const auto operatingMode = static_cast<u8>(om);
+    const auto bcdBinaryMode = static_cast<u8>(bcdbin);
+
+    const u8 command = bcdBinaryMode | operatingMode << 1 | accessMode << 4 | channel << 6;
+    kprintln("Sending PIT command: 0x%X", command);
+    IO::outb(PIT_CMD_REGISTER, command);
+
+    const auto count = count_for_frequency(frequency);
+
+    kprintln("Sending PIT count: %d for frequency %d Hz", count, frequency);
+    set_count(c, count);
 }
 
-void PIT::disable() {
+void PIT::set_count(Channel c, u32 count) {
+    InterruptScope _;
+
+    u16 channel = PIT_CHANNEL_0 + static_cast<u8>(c);
+
+    IO::outb(channel, count & 0xFF);
+    IO::outb(channel, (count & 0xFF00) >> 8);
+}
+
+u32 PIT::count_for_frequency(u32 f) {
+    if (f < 20 || f > PIT_OSCILLATOR_FREQ) {
+        panic("PIT cannot produce requested frequency (20..=1193182)");
+    }
+
+    return PIT_OSCILLATOR_FREQ / f;
+}
+
+u32 PIT::read_count(Channel c) {
+    InterruptScope _;
+
+    u16 channel = PIT_CHANNEL_0 + static_cast<u8>(c);
+    u32 count = 0;
+
+    IO::outb(PIT_CMD_REGISTER, 0);
+
+    count = IO::inb(channel);
+    count |= IO::inb(channel) << 8;
+
+    return count;
 }
 
 }
