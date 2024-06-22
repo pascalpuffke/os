af76ef2faf6762fe8c90d94c664747c8af68f988
pascalpuffke <pascal@pascalpuffke.de>
2024-06-22 16:22:50 +0200

    stdlib: Check sizes of types

--- include/stdlib/types.h
+++ include/stdlib/types.h
@@ -4,14 +4,22 @@
 #include <stdint.h>
 
 using u8 = uint8_t;
+static_assert(sizeof(u8) == 1);
 using u16 = uint16_t;
+static_assert(sizeof(u16) == 2);
 using u32 = uint32_t;
+static_assert(sizeof(u32) == 4);
 using u64 = uint64_t;
+static_assert(sizeof(u64) == 8);
 
 using i8 = int8_t;
+static_assert(sizeof(i8) == 1);
 using i16 = int16_t;
+static_assert(sizeof(i16) == 2);
 using i32 = int32_t;
+static_assert(sizeof(i32) == 4);
 using i64 = int64_t;
+static_assert(sizeof(i64) == 8);
 
 using usize = size_t;
 using isize = intptr_t;
